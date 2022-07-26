4ce68c1a0d077092fc1e2c68060e5c204a3a0dd5
Dustin Thurston <dthurston1996@gmail.com>
2022-07-25 23:39:40 -0400

    Info displayed on screen
    Queen of Sauce displays in texturebox when hovering over cookable recipe player has not cooked yet

--- UIInfoSuite2/UIElements/ShowHoverCraftableInCollection.cs
+++ UIInfoSuite2/UIElements/ShowHoverCraftableInCollection.cs
@@ -1,7 +1,9 @@
-﻿using StardewModdingAPI;
+﻿using Microsoft.Xna.Framework;
+using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
+using StardewValley.Menus;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +23,16 @@ namespace UIInfoSuite2.UIElements
         {
             _helper = helper;
         }
+
+        private readonly ClickableTextureComponent _shippingTopIcon =
+            new(
+                "",
+                new Rectangle(0, 0, Game1.tileSize, Game1.tileSize),
+                "",
+                "",
+                Game1.mouseCursors,
+                new Rectangle(134, 236, 30, 15),
+                Game1.pixelZoom);
 
         public void ToggleOption(bool showItemHoverInformation)
         {
@@ -49,13 +61,65 @@ namespace UIInfoSuite2.UIElements
 
         private void OnRendered(object sender, EventArgs e)
         {
-            if(Game1.activeClickableMenu != null)
+            DrawToolTip();
+        }
+
+        private void DrawToolTip()
+        {
+            if (Game1.activeClickableMenu != null)
             {
-                if(_hoverRecipe.Value is StardewValley.CraftingRecipe recipe)
+                if (_hoverRecipe.Value is StardewValley.CraftingRecipe recipe)
                 {
-                    if (recipe.isCookingRecipe && Game1.player.recipesCooked.ContainsKey(recipe.getIndexOfMenuView()))
+                    if (recipe.isCookingRecipe && !Game1.player.recipesCooked.ContainsKey(recipe.getIndexOfMenuView()))
                     {
-                        ModEntry.MonitorObject.Log($"Player has cooked {recipe.name}", LogLevel.Debug);
+                        ModEntry.MonitorObject.Log($"Player has not cooked {recipe.name}", LogLevel.Debug);
+
+                        int windowHeight = 80;
+
+                        int windowY = Game1.getMouseY() + 20;
+                        windowY = Game1.viewport.Height - windowHeight - windowY < 0 ? Game1.viewport.Height - windowHeight : windowY;
+
+                        int windowWidth = 80;
+
+                        int windowX = Game1.getMouseX() - windowWidth - 25;
+
+                        if (Game1.getMouseX() > Game1.viewport.Width - 300)
+                        {
+                            windowX = Game1.viewport.Width - windowWidth - 350;
+                        }
+                        else if (windowX < 0)
+                        {
+                            windowX = Game1.getMouseX() + 350;
+                        }
+
+                        Vector2 windowPos = new Vector2(windowX, windowY);
+
+                        int num1 = (int)windowPos.X + 22;
+                        int num2 = (int)windowPos.Y + 22;
+
+                        //_shippingTopIcon.bounds.X = num1;
+                        //_shippingTopIcon.bounds.Y = num2;
+                        //_shippingTopIcon.scale = 1.2f;
+                        //_shippingTopIcon.draw(Game1.spriteBatch)
+
+                        IClickableMenu.drawTextureBox(
+                            Game1.spriteBatch,
+                            Game1.menuTexture,
+                            new Rectangle(0, 256, 60, 60),
+                            (int)windowPos.X,
+                            (int)windowPos.Y,
+                            windowWidth,
+                            windowHeight,
+                            Color.White);
+
+
+                        var _icon = new ClickableTextureComponent(
+                            new Rectangle(num1, num2, 40, 40),
+                            Game1.mouseCursors,
+                            new Rectangle(609, 361, 28, 28),
+                            1.3f);
+
+                        _icon.draw(Game1.spriteBatch);
                     }
                 }
             }
